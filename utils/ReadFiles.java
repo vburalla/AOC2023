b30d6fee835e6bf11d3afe1ca111ceb0f5eb43be
victor <victorburalla@gmail.com>
2023-12-01 18:49:25 +0100

    first day complete

---
+++ utils/ReadFiles.java
@@ -0,0 +1,20 @@
+package utils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class ReadFiles {
+
+    public static List<String> getInputData(String inputFile) {
+
+        try{
+            return Files.readAllLines(Paths.get(inputFile));
+        } catch (IOException ex) {
+            System.out.println("Exception" + ex.getLocalizedMessage());
+        }
+        return null;
+    }
+}
+
