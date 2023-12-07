4471c69d9b299c2ce7a3409c6a990a4ae7ebe21e
vburalla <100727742+vburalla@users.noreply.github.com>
2023-12-07 15:53:08 +0000

    reupload day1 and add numberutils

---
+++ utils/NumberUtils.java
@@ -0,0 +1,9 @@
+package utils;
+
+import java.util.Map;
+
+public class NumberUtils {
+
+    public static final Map<String, String> WORD_NUMBER_VALUES = Map.of("one","1", "two", "2", "three", "3", "four", "4", "five", "5", "six", "6", "seven", "7", "eight", "8", "nine", "9");
+    public static final Map<String, String> NUMBER_NAMES = Map.of("1","one","2", "two", "3","three", "4",  "four", "5", "five", "6","six", "7",  "seven", "8", "eight", "9", "nine");
+}
