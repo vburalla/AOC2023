0c34b705990e2bd8b067ceebd0af0b819d82c01e
victor <victorburalla@gmail.com>
2023-12-13 18:15:02 +0100

    day12

--- day12/Arrangement.java
+++ day12/Arrangement.java
@@ -1,2 +1,43 @@
-package day12;public class Arrangement {
+package day12;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Arrangement {
+
+    private String arrange;
+
+    public String getArrange() {
+        return arrange;
+    }
+
+    public String getLongArrange() {
+        StringBuilder sb = new StringBuilder(this.arrange);
+        for(int i=0; i<4; i++) {
+            sb.append("?").append(this.arrange);
+        }
+        return sb.toString();
+    }
+
+    public List<Integer> getLongSizes() {
+        List<Integer> longSizes = new ArrayList<>();
+        for(int i=0; i<5; i++) {
+            longSizes.addAll(this.sizes);
+        }
+        return longSizes;
+    }
+
+    public List<Integer> getSizes() {
+        return sizes;
+    }
+
+    private List<Integer> sizes;
+
+    public Arrangement(String text) {
+        var parts = text.split(" ");
+        arrange  = parts[0];
+        sizes = Arrays.stream(parts[1].split(",")).map(c -> Integer.valueOf(c)).collect(Collectors.toList());
+    }
 }
--- day12/Day12.java
+++ day12/Day12.java
@@ -1,2 +1,50 @@
-package day12;public class Day12 {
+package day12;
+
+import utils.ReadFiles;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Day12 {
+
+    public static void main(String... args) {
+
+        var lines = ReadFiles.getInputData("day12/input.txt");
+        List<Arrangement> arrangements = new ArrayList<>();
+        Long solutions = 0L;
+        Long solutions2 = 0L;
+
+        for (String line : lines) {
+            Arrangement arrangement = new Arrangement(line);
+            solutions += getPossibleSolutions(arrangement.getArrange()+".", arrangement.getSizes(), 0);
+            solutions2 += getPossibleSolutions(arrangement.getLongArrange()+".", arrangement.getLongSizes(), 0);
+        }
+        System.out.println(solutions);
+        System.out.println(solutions2);
+    }
+
+    private static Long getPossibleSolutions(String remainingArrange, List<Integer> remainingSizes, Integer builtGroups) {
+
+        if (remainingArrange == null || remainingArrange.equals(""))
+            return remainingSizes.isEmpty() && remainingArrange.equals("") ? 1L : 0L;
+
+        Long solutions = 0L;
+
+        var options = (remainingArrange.charAt(0) == '?') ? List.of('.', '#') : List.of(remainingArrange.charAt(0));
+        for (char c : options) {
+            if (c == '#')
+                solutions += getPossibleSolutions(remainingArrange.substring(1), remainingSizes, builtGroups + 1);
+            else {
+                if (builtGroups != null && builtGroups > 0) {
+                    if (!remainingSizes.isEmpty() && remainingSizes.get(0).equals(builtGroups)) {
+                        List<Integer> sublist = remainingSizes.subList(1, remainingSizes.size());
+                        solutions += getPossibleSolutions(remainingArrange.substring(1), sublist, 0);
+                    }
+                } else
+                    solutions += getPossibleSolutions(remainingArrange.substring(1), remainingSizes, 0);
+            }
+        }
+        return solutions;
+    }
+
 }
