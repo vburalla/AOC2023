c918fbde15780f8c158607710e4b1f07cc71e117
Víctor Buralla Blasco <vburalla@mercadona.es>
2023-12-14 15:10:48 +0100

    day13 complete

--- day13/Day13.java
+++ day13/Day13.java
@@ -1,13 +1,8 @@
 package day13;
 
-import utils.Point;
 import utils.ReadFiles;
-
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.BitSet;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Day13 {
 
@@ -16,11 +11,8 @@ public class Day13 {
         Integer sum2 = 0;
         var lines = ReadFiles.getInputData("day13/input.txt");
         List<String> patternLines = new ArrayList<>();
-        int i = 0;
         for (String line : lines) {
             if (line.equals("")) {
-                i++;
-                System.out.println("Pattern " + i);
                 sum += getResult(patternLines);
                 sum2 += getResultWithSmudge(patternLines);
                 patternLines.clear();
@@ -34,15 +26,16 @@ public class Day13 {
         System.out.println(sum2);
     }
 
-    public static Point findSmudge2(List<String> patterns) {
+    public static Integer findSmudge2(List<String> patterns) {
 
         int i = 0;
         int j = 0;
         int k = 0;
-        int row = 0;
+        int row;
         boolean charSwapped = false;
         boolean equalFound = true;
         int charDifferencePosition = -1;
+        int startRow = 0;
 
         while (i < patterns.size() - 1) {
             charSwapped = false;
@@ -53,6 +46,7 @@ public class Day13 {
                     row = i;
                     equalFound = true;
                 }
+                startRow = i;
                 j = i - 1;
                 k = i + 2;
                 while (j >= 0 && k < patterns.size() && ((equalFound = patterns.get(j).equals(patterns.get(k))) ||
@@ -66,89 +60,24 @@ public class Day13 {
                     k++;
                 }
                 if (equalFound && (j < 0 || k >= patterns.size()) && charSwapped) {
-                    return new Point(row, charDifferencePosition);
+                    return startRow + 1;
                 }
                 charSwapped = false;
             }
             i++;
         }
-        return null;
-    }
-
-    public static Point findSmudge(List<String> patterns) {
-
-        int i = 0;
-        int j = 0;
-        int k = 0;
-        int charPosition = 0;
-        boolean found = false;
-        boolean foundSmudge = false;
-        Point point = null;
-
-        while (i < patterns.size() - 1 && !foundSmudge) {
-            if (patterns.get(i).equals(patterns.get(i + 1))) {
-                found = true;
-                j = i - 1;
-                k = i + 2;
-                boolean swapped = false;
-                while (found && j >= 0 && k < patterns.size()) {
-                    found = patterns.get(j).equals(patterns.get(k));
-                    if (!found && (charPosition = charDifference(patterns.get(j), patterns.get(k))) >= 0 && !swapped) {
-                        swapped = true;
-                        point = new Point(j, charPosition);
-                    }
-                    j--;
-                    k++;
-                }
-                if (swapped && found)
-                    return point;
-            } else {
-                if ((charPosition = charDifference(patterns.get(i), patterns.get(i + 1))) >= 0) {
-                    j = i - 1;
-                    k = i + 2;
-                    found = true;
-                    while (j >= 0 && k < patterns.size() && found) {
-                        found = patterns.get(j).equals(patterns.get(k));
-                        j--;
-                        k++;
-                    }
-                    if (found)
-                        return new Point(i + 1, charPosition);
-                }
-            }
-            i++;
-        }
-        return null;
-    }
-
-    private static List<String> updateLinesWithSmudge(List<String> lines, Point point) {
-        List<String> lines2 = lines.stream().collect(Collectors.toList());
-        var line = lines2.get(point.getRow()).toCharArray();
-        line[point.getColumn()] = line[point.getColumn()] == '#' ? '.' : '#';
-        lines2.set(point.getRow(), new String(line));
-        return lines2;
+        return -1;
     }
 
     private static Integer getResultWithSmudge(List<String> lines) {
-
-        Integer total = 0;
-        Point smudge = findSmudge2(lines);
-        if (smudge != null) {
-            var lines2 = updateLinesWithSmudge(lines, smudge);
-            var result = analyzeMirror(lines2);
-            //total = result != null? 100 * analyzeMirror(lines2) : 0;
+        Integer result = findSmudge2(lines);
+        if(result >= 0) {
+            return 100 * result;
+        } else {
+            lines = transpose(lines);
+            return findSmudge2(lines);
         }
-
-        lines = transpose(lines);
-        smudge = findSmudge2(lines);
-        if (smudge != null) {
-            lines = updateLinesWithSmudge(lines, smudge);
-            var result = analyzeMirror(lines);
-            total = result != null? total + result : total;
-        }
-        return total;
     }
-
 
     private static Integer getResult(List<String> lines) {
 
